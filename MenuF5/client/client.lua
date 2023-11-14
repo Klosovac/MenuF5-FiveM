8166378f11e87edd4f7d1131771944660a3a5e73
Yanis.lua <143359755+Klosovac@users.noreply.github.com>
2023-11-14 08:29:12 +0100

    Mise à jour du fichier Menu-F5/client/client.lua

--- MenuF5/client/client.lua
+++ MenuF5/client/client.lua
@@ -1144,7 +1144,7 @@ RegisterCommand("discord",function()
 end)
 
 function discordapp()
-	ESX.ShowNotification("~g~Bienvenue~s~ \nVoici le lien Discord du serveur : ~o~https://discord.gg/Five-Dev")
+	ESX.ShowNotification("~g~Bienvenue~s~ \nVoici le lien Discord du serveur : ~o~https://discord.gg/discord.fr")
 end
 
 --- DP
